37593cf33622462b0dd03f9917d34dd95ddd065a
Hamish Gibbs <hamishgibbs@gmail.com>
2021-01-15 14:34:14 +0000

    add test

--- tests/test_aggregate.py
+++ tests/test_aggregate.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import pytest
+from datetime import datetime
 
 from fbutils import aggregate
 
@@ -21,16 +22,35 @@ def default_time_movement():
     return mob
 
 
+@pytest.fixture
+def default_daily_movement():
+
+    mob = pd.DataFrame(
+        {
+            "date_time": [datetime(2020, 3, 10),
+                          datetime(2020, 3, 11),
+                          datetime(2020, 3, 12)],
+            "start_quadkey": ["1", "1", "1"],
+            "end_quadkey": ["1", "1", "1"],
+            "journey": ["1_1", "1_1", "1_1"],
+            "n_crisis": [1, 2, 3],
+            "n_baseline": [2, 2, 3],
+        }
+    )
+
+    return mob
+
+
 def test_aggregate_tile_movement_columns(default_time_movement):
 
-    res = aggregate.aggregate_tile_movement(default_time_movement)
+    res = aggregate.aggregate_tile_movement(default_time_movement, time_agg='daily')
 
     assert "perc_change" in res.columns
 
 
 def test_aggregate_tile_movement_records(default_time_movement):
 
-    res = aggregate.aggregate_tile_movement(default_time_movement)
+    res = aggregate.aggregate_tile_movement(default_time_movement, time_agg='daily')
 
     assert len(res.index) == 2
 
@@ -39,3 +59,21 @@ def test_aggregate_tile_movement_errors(default_time_movement):
 
     with pytest.raises(Exception):
         aggregate.aggregate_tile_movement(default_time_movement, "anything")
+
+
+def test_aggregate_tile_movement_weekly(default_daily_movement):
+
+    res = aggregate.aggregate_tile_movement(default_daily_movement, time_agg='weekly')
+
+    print(res)
+
+    assert len(res.index) == 1
+
+
+def test_date_to_weekly():
+
+    date = datetime(2020, 3, 10)
+
+    res = aggregate.date_to_weekly(date)
+
+    assert res == datetime(2020, 3, 9)
